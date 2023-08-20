fec787f637c2bf3ef8a6ac64267b6be3d9f31723
walmonn <wagner.a.monte@hotmail.com>
2023-08-20 11:11:09 -0300

    commit_004

---
+++ cap_02/2.4.py
@@ -0,0 +1,8 @@
+# Escreva um programa que exiba o resultado de 2a x 3b, em que a vale 3 e b vale 5.
+
+va = int(input('Digite o Valor de (A): '))
+vb = int(input('Digite o Valor de (B): '))
+
+soma = (2*va) * (2*vb)
+
+print('O Resultado da Equação é {}'.format(soma))
