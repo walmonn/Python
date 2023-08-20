501cc64a46c08bbe0c18c32a74194499a27c5989
walmonn <wagner.a.monte@hotmail.com>
2023-08-20 10:43:40 -0300

    commit_01

---
+++ cap_02/2.1.py
@@ -0,0 +1,10 @@
+#Converta as seguintes expressões matemáticas para que possam ser calculadas usando o interpretador Python.
+
+exp01 = 10 + 20 * 30
+print(exp01)
+
+exp02 = 4**2 * 30
+print(exp02)
+
+exp03 = (9**4 + 2) * 6 - 1
+print(exp03)
