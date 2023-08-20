dfe080a219d4782109f54733803bcdc8d67b3c73
walmonn <wagner.a.monte@hotmail.com>
2023-08-20 10:51:32 -0300

    commit_002

---
+++ cap_02/2.2.py
@@ -0,0 +1,10 @@
+#Digite a seguinte expressão no interpretador:
+
+exp = 10 % 3 * 10 ** 2 + 1 - 10 * 4 / 2
+print(exp)
+
+#Tente resolver o mesmo cálculo, usando apenas lápis e papel. Observe como a prioridade das operações é importante.
+
+#1. Exponenciação ou potenciação(**).
+#2. Multiplicação (*), Divisão (/ e //) e módulo (%).
+#3. Adição (+) e Subtração (-).
