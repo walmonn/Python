f0b77340690ed4e2c0de4d7408202f8b38c73d69
walmonn <wagner.a.monte@hotmail.com>
2023-08-21 20:11:12 -0300

    commit_008

---
+++ cap_03/3.2.py
@@ -0,0 +1,27 @@
+# Complete a tabela a seguir, respondendo True ou False.
+# Considere a = 4, b = 10, c = 5.0, d = 1 e f = 5.
+
+# Expressão     Resultado
+# a == c        [] True [X] False   
+# a < b         [X] True [] False 
+# d > b         [] True [X] False
+# c != f        [] True [X] False 
+# a == b        [] True [X] False
+# c < d         [] True [X] False
+# b > a         [X] True [] False
+# c >= f        [X] True [] False
+# f >= c        [X] True [] False
+# c <= c        [X] True [] False 
+# c <= f        [X] True [] False 
+
+# False (a==c)
+# True (a<b)
+# False (d>b)
+# False (c!=f)
+# False (a==b)
+# False (c<d)
+# True (b>a)
+# True (c>=f)
+# True (f>=c)
+# True (c<=c)
+# True (c<=f)
