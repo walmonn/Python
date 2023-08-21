5173586e6cfbd9eaaebdca959c76409607543cfe
walmonn <wagner.a.monte@hotmail.com>
2023-08-21 19:50:20 -0300

    commit_007

---
+++ cap_03/3.1.py
@@ -0,0 +1,8 @@
+# Complete a Tabela a seguir, marcando inteiro ou ponto flutuante dependendo do número apresentado.
+
+# N1 = 5       [X] Inteiro [] Ponto Flutuante
+# N2 = 5.0     [] Inteiro [X] Ponto Flutuante
+# N3 = 4.3     [] Inteiro [X] Ponto Flutuante
+# N4 = -2      [X] Inteiro [] Ponto Flutuante
+# N5 = 100     [X] Inteiro [] Ponto Flutuante
+# N6 = 1.333   [] Inteiro [X] Ponto Flutuante
