8d259d23467164cbfad898c5c13eb2e4516cbaaa
walmonn <wagner.a.monte@hotmail.com>
2023-08-21 19:23:35 -0300

    commit_006

--- cap_02/2.6.py
+++ cap_02/2.6.py
@@ -3,4 +3,5 @@
 salario = float(input('Digite o seu Salário: '))
 aumento = (salario * 15) / 100
 salario_real = salario + aumento
+
 print('O Valor do Salário com aumento é de {}'.format(salario_real))
