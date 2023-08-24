cff5118e8ebf54f63d455729f64034b60253e454
walmonn <wagner.a.monte@hotmail.com>
2023-08-24 18:21:29 -0300

    commit_009

---
+++ cap_03/3.3.py
@@ -0,0 +1,17 @@
+# Complete a tabela a seguir utilizando a = True, b = False e c = True
+
+# Expressão        Resultado
+
+# a and a          True 
+# b and b          False
+# not c            False
+# not b            True
+# not a            False
+# a and b          False
+# b and c          False
+# a or c           True
+# b or c           True
+# c or a           True
+# c or b           True
+# c or c           True
+# b or b           False
