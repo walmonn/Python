17cd7915065bed87001eda35160ad51ae313b6e6
walmonn <wagner.a.monte@hotmail.com>
2023-08-26 17:34:34 -0300

    commit_011

---
+++ cap_03/3.5.py
@@ -0,0 +1,7 @@
+# Calcule o resultado da expressão A > B and C or D, utilizando os valores da tabela a seguir.
+
+# A     B     C        D     Resultado     
+
+# 1     2     True     False     F 
+# 10    3     False    False     F
+# 5     1     True     True      T
