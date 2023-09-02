f87dfc429794eb6b170b5369cebbc730f2ab0929
walmonn <wagner.a.monte@hotmail.com>
2023-09-02 11:31:12 -0300

    commit_013

--- cap_03/3.7.py
+++ cap_03/3.7.py
@@ -1,5 +1,6 @@
+# Faça um programa que peça dois números inteiros. Imprima a soma desses dois numeros na tela.
+
 n1 = int(input("Digite um número inteiro: "))
 n2 = int(input("Digite outro número inteiro: "))
 soma = n1 + n2
 print(f"A Soma entre {n1} e {n2} é igual a {soma}")
-
