5cbdcf445ea9592224b525e3650ca22b30c43bc3
walmonn <wagner.a.monte@hotmail.com>
2023-09-02 11:42:11 -0300

    commit_014

---
+++ cap_03/3.8.py
@@ -0,0 +1,5 @@
+# Escreva um programa que leia um valor em metros e o exiba convertido em milimetros.
+
+metros = float(input("Digite o valor de metros que gostaria de converter em Milímetros: "))
+mili = metros * 1000
+print(f"{metros:.2f} convertido é {mili:.2f} Milímetros")
