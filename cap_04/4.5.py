6f85e4a9663a50d1f5da09b5b29007cf6b392798
walmonn <wagner.a.monte@hotmail.com>
2023-09-22 13:41:15 -0300

    commit_4.5

---
+++ cap_04/4.5.py
@@ -0,0 +1,8 @@
+#Execute o Programa 4.4 e experimente alguns valores. Verifique se os resultados foram os mesmos do Programa 4.2.
+
+idade = int(input("Digite a idade de seu carro: "))
+if idade <= 3:
+    print("Seu Carro é Novo")
+else:
+    print("Seu Carro é Velho")
+    
